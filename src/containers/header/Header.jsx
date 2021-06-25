abc4fbb4ecce8a9a759dd2e5d0fa723d6fc7a394
Uri Dilewsky <stranger354999@gmail.com>
2021-06-25 10:50:45 +0600

    header maked

---
+++ src/containers/header/Header.jsx
@@ -0,0 +1,16 @@
+import './header.sass';
+
+function Header() {
+  return (
+    <section className="header header--margin">
+      <div className="container header__container">
+        <h1 className="header__title">What you’ll learn</h1>
+        <p className="header__text">Create beautiful webflow sites with flowbase. Clone for
+          free and get started building a site today.
+        </p>
+      </div>
+    </section>
+  )
+}
+
+export default Header
